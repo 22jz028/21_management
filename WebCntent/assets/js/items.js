3a382ba7fce520ddb6196a289b83b4f70da28518
22jz028 <146900699+22jz028@users.noreply.github.com>
2024-02-09 05:33:51 +0900

    Add files via upload
    WebCntent 一部

---
+++ WebCntent/assets/js/items.js
@@ -0,0 +1,57 @@
+
+import {ajax} from "./lib/module.js";
+
+(function () {
+	const elItemid = document.querySelector('#item-id');
+	const elStck = document.querySelector('#item-stock');
+	const elstockupdate = document.querySelector('#item-update');
+    ajax('/management/ItemList')
+        .then(res => {
+            let items = res;
+            console.log("script.js:", items);
+
+            const elList = document.querySelector('.list-item');
+            console.log(elList);
+
+            // テーブルのヘッダーを一度だけ表示
+            const elTable = document.createElement('table');
+            elTable.innerHTML = `
+                <tr>
+                	<th>商品コード</th>
+                    <th>商品名</th>
+                    <th>商品価格</th>
+                    <th>商品説明</th>
+                    <th>素材</th>
+                    <th>サイズ名</th>
+                </tr>
+            `;
+            elList.appendChild(elTable);
+
+            // 各アイテムを追加
+            items.forEach(list => {
+                const elItem = document.createElement('tr');
+                elItem.innerHTML = `
+                    <th>${list.id}</th>
+                    <th>${list.product_name}</th>
+                    <th>${list.price}円</th>
+                    <th>${list.description}</th>
+                    <th>${list.material}</th>
+                    <th>${list.sizename}</th>
+
+                `;
+
+                // 新しい行をテーブルに追加
+            elTable.appendChild(elItem);
+            });
+            elstockupdate.addEventListener('click', () =>{
+				const obt = {
+					id : elItemid.value,
+					stock : elStck.value
+				};
+				ajax('http://localhost:8080/management/stoc', 'post', obt)
+				.then(res =>{
+					alert('追加しました');
+				})
+			})
+		})
+})();
